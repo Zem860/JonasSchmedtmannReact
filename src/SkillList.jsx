9ad63240d06c090525a55763652bcd7650477ce6
Zem860 <zemmyhammy@gmail.com>
2024-11-02 14:59:15 +0800

    update:map skills data with ternary to decide icon with levels

--- src/SkillList.jsx
+++ src/SkillList.jsx
@@ -1,16 +1,20 @@
-import Skill from "./Skill";
+import Skill from './Skill';
+import {skills} from './data.js';
 const SkillList = () => {
   return (
     <ul className="skill-list">
-        {/* ["", "", "", "", "", "Svelte"] */}
-        <Skill skill={"HTML+CSS"} color={"#0153b0"} icon={"💪"}/>
-        <Skill skill={"JavaScript"} color={"#fcf000"} icon={"💪"}/>
-        <Skill skill={"Web Design"} color={"#7deb00"} icon={"👍"}/>
-        <Skill skill={"Git and GitHub"} color={"#fc5757"} icon={"👍"}/>
-        <Skill skill={"React"} color={"#00dcd6"} icon={"👍"}/>
-        <Skill skill={"React"} color={"#f0a700"} icon={"🔥"}/>
+      {skills.map((d, index) => (
+        <Skill key={index} skill={d.skill} color={d.color} icon={d.level ==="advanced"?"💪":d.level==='intermediate'?"👍":"🔥"} />
+      ))}
     </ul>
-  );
-};
+  // <ul className="skill-list">
+  //     <Skill skill={"HTML+CSS"} color={"#0153b0"} icon={"💪"}/>
+  //     <Skill skill={"JavaScript"} color={"#fcf000"} icon={"💪"}/>
+  //     <Skill skill={"Web Design"} color={"#7deb00"} icon={"👍"}/>
+  //     <Skill skill={"Git and GitHub"} color={"#fc5757"} icon={"👍"}/>
+  //     <Skill skill={"React"} color={"#00dcd6"} icon={"👍"}/>
+  //     <Skill skill={"React"} color={"#f0a700"} icon={"🔥"}/>
+  // </ul>
+)};
 
 export default SkillList;
