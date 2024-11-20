39ec0d55ff71bc36975c6e9e13144e2df73d1522
Zem860 <zemmyhammy@gmail.com>
2024-11-20 08:40:11 +0800

    coding challenging complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-function App() {
+import useGeolocation from "./useGeolocation";
 
+export default function App() {
+  
+  const { lat, lng, isLoading, countClicks, error, getPosition } = useGeolocation();
+
+  
   return (
-    <>
+    <div>
+      <button onClick={getPosition} disabled={isLoading}>
+        Get my position
+      </button>
 
-    </>
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
   );
 }
-
-
-
-export default App;
