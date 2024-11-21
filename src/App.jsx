73e28d00b8c09642903481eb559252bfd45d3f20
Zem860 <zemmyhammy@gmail.com>
2024-11-21 09:40:13 +0800

    add jsonserver command and useReducer basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        question: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status:'error'
+      }
+    default:
+      throw new Error("the action is unknown")
+  }
+};
 function App() {
-
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch('http://localhost:8080/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((error) => dispatch({type:'dataFailed'}));
+  }, []);
   return (
-    <>
-
-    </>
+    <div className="app">
+      <Header />
+    </div>
   );
 }
-
-
 
 export default App;
